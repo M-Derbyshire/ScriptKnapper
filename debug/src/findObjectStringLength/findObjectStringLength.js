e9d1af51dc79f76e0f6476ac161cd6d00885e26d
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-01-25 12:26:04 +0000

    wrote and tested the findObjectStringLength function

---
+++ debug/src/findObjectStringLength/findObjectStringLength.js
@@ -0,0 +1,59 @@
+import isCharacterEscaped from '../isCharacterEscaped/isCharacterEscaped';
+
+/*
+    Inputs:
+        - objectText: The string of text that will contain the 
+                    object. This string should begin with the 
+                    opening brace. The opening brace will have
+                    already been checked to make sure it isn't
+                    escaped, however any other opening brace
+                    within the object will be checked here.
+            
+    Output:
+        - This function will return the length of the substring
+            that makes up this object (including the handlebar 
+            braces).
+        - If the object in the string is not complete, or it
+            doesn't contain an object, this function will 
+            return -1.
+*/
+
+function findObjectStringLength(objectText)
+{
+    if(objectText.length < 2 || objectText.charAt(0) !== "{")
+    {
+        return -1;
+    }
+    
+    
+    let layer = 0; // There may be objects in the object, so
+                    // what "layer" are we on?
+    let currentChar = "";
+    let i = 0; // We want to use this later when returning
+    while(i < objectText.length)
+    {
+        currentChar = objectText.charAt(i);
+        
+        if(currentChar == "{" && !isCharacterEscaped(objectText.substring(0, i + 1)))
+        {
+            layer++;
+        }
+        else if (currentChar == "}" && !isCharacterEscaped(objectText.substring(0, i + 1)))
+        {
+            layer--;
+        }
+        
+        i++; //In the last iteration of the loop, this will become the length to return.
+    }
+    
+    if(layer > 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return i;
+    }
+}
+
+export default findObjectStringLength;
---
+++ debug/src/findObjectStringLength/findObjectStringLength.test.js
@@ -0,0 +1,68 @@
+import findObjectStringLength from './findObjectStringLength';
+
+test("findObjectStringLength will return -1 if given an incomplete object string", ()=> {
+    
+    const objectText = "{ 'data': { 'a': 'hello', 'b': 'goodbye' }";
+    
+    const result = findObjectStringLength(objectText);
+    
+    expect(result).toBe(-1);
+    
+});
+
+test("findObjectStringLength will return -1 if given an empty string, or one without any braces", ()=> {
+    
+    const objectText1 = "hello";
+    const objectText2 = "";
+    
+    const result1 = findObjectStringLength(objectText1);
+    const result2 = findObjectStringLength(objectText2);
+    
+    expect(result1).toBe(-1);
+    expect(result2).toBe(-1);
+    
+});
+
+// -------------------------------------------------------------------------------------
+
+test("findObjectStringLength will return the correct length for a given single-layered object string", () => {
+    
+    const objectText = "{ 'foo': 'hello', 'bar': 'bye' }";
+    
+    const result = findObjectStringLength(objectText);
+    
+    expect(result).toBe(objectText.length);
+    
+});
+
+test("findObjectStringLength will return the correct length for a given multi-layered object string", () => {
+    
+    const objectText = "{ 'foo': { 'bar': { 'baz': 'hello' } } }";
+    
+    const result = findObjectStringLength(objectText);
+    
+    expect(result).toBe(objectText.length);
+    
+});
+
+// -----------------------------------------------------------------------------------------
+
+test("findObjectStringLength will return the correct length for an object string containing escaped braces", () => {
+    
+    const objectText = String.raw`{ 'foo': { 'bar': '\{' } }`;
+    
+    const result = findObjectStringLength(objectText);
+    
+    expect(result).toBe(objectText.length);
+    
+});
+
+test("findObjectStringLength will return -1 for an object string where an escaped brace makes the object invalid", () => {
+    
+    const objectText = String.raw`{ 'foo': { 'bar': 'hello' \} }`;
+    
+    const result = findObjectStringLength(objectText);
+    
+    expect(result).toBe(-1);
+    
+});
