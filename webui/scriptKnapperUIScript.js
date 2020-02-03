e27850828e23623abd12ea060a884831344446ed
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-02-03 22:01:28 +0000

    coded the string manipulation tool and tried it out

--- webui/scriptKnapperUIScript.js
+++ webui/scriptKnapperUIScript.js
@@ -4,7 +4,7 @@
 // textarea.
 function transpileClickAction()
 {
-    let skMain = scriptKnapperMain; //In the scriptKnapperMain library
+    let skMain = scriptKnapperMain; //In the scriptKnapper library
     
     let templateInput = document.getElementById("templateInput");
     let markupInput = document.getElementById("markupInput");
@@ -23,3 +23,60 @@ function transpileClickAction()
     scriptResultContainer.style.backgroundColor = (resultIsError) ? errorColor : successColor;
 }
 
+// The onClick method for the Prepare a String tool's button.
+// This will use the replaceSubstrings function in the
+// library to make the requested changes to the provided
+// string.
+function prepareStringClickAction()
+{
+    let skReplaceSubStrings = replaceSubstrings; //In the scriptKnapper library
+    let stringInput = document.getElementById("prepareStringInput");
+    let stringOutput = document.getElementById("prepareStringOutput");
+    
+    
+    let whitespaceDropdown = document.getElementById("prepareStringWhitespaceDropdown");
+    let escapeDoubleQuotes = document.getElementById("prepareStringEscapeDoubleQuotes");
+    let replaceHandlebars = document.getElementById("prepareStringReplaceHandlebars");
+    
+    let replacementOptions = [];
+    
+    //Add options to be given to the skReplaceSubStrings function
+    if(whitespaceDropdown.options[whitespaceDropdown.selectedIndex].value === "spaceReplace")
+    {
+        replacementOptions.push(
+            { from: "\t", to: " " },
+            { from: "\v", to: " " },
+            { from: "\r", to: " " },
+            { from: "\n", to: " " },
+        );
+    }
+    
+    if(whitespaceDropdown.options[whitespaceDropdown.selectedIndex].value === "escapeCodeReplace")
+    {
+        replacementOptions.push(
+            { from: "\t", to: String.raw`\t` },
+            { from: "\v", to: String.raw`\v` },
+            { from: "\r", to: String.raw`\r` },
+            { from: "\n", to: String.raw`\n` },
+        );
+    }
+    
+    if(escapeDoubleQuotes.checked)
+    {
+        replacementOptions.push(
+            { from: '"', to: String.raw`\"` }
+        );
+    }
+    
+    if(replaceHandlebars.checked)
+    {
+        replacementOptions.push(
+            { from: "{", to: "@ohb" },
+            { from: "}", to: "@chb" }
+        );
+    }
+    
+    //Now run the string through the skReplaceSubStrings function,
+    //and output to the prepareStringOutput textarea
+    stringOutput.value = skReplaceSubStrings(stringInput.value, replacementOptions);
+}
