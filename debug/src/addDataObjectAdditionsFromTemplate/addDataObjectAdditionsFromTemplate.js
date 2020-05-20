7339a0222a99ee09af91205cc881d3a616978b99
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-05-20 17:18:36 +0100

    created (along with tests) the addDataObjectAdditionsFromTemplate() function for resolving data addition tags in templates. This won't remove the tags, so we need another function for that. All tests now pass fro this function

---
+++ debug/src/addDataObjectAdditionsFromTemplate/addDataObjectAdditionsFromTemplate.js
@@ -0,0 +1,62 @@
+import mergeObjects from '../mergeObjects/mergeObjects';
+import findObjectStringLength from '../findObjectStringLength/findObjectStringLength';
+
+/*
+    Inputs:
+        - The current data object, as a single object.
+        - The current template, as a string.
+            
+    Output:
+        - If there is an error, the function will return an array. The first indes will be true,
+            to say there has been an error, and the second will be the error text.
+        - If there is not an error, the function will return an array. The first index will be false,
+            to say there hasn't been an error, and the second will be the new version of the data 
+            object, with the added properties.
+*/
+
+function addDataObjectAdditionsFromTemplate(dataObject, template)
+{
+     //The first part of the string to feed into prepareErrorMessage()
+    let errPreText= "ScriptKnapper encountered an issue when attempting to add data from a template to the data object: ";
+    let braceIndex; //The index of the first brace in the substring that is being searched for braces
+    
+    //Will be cut down (from the front onwards) to avoid re-adding already added data
+    let currentlyUnsearchedString = template;
+    
+    while (currentlyUnsearchedString.includes("{+"))
+    {
+        braceIndex = currentlyUnsearchedString.indexOf("{+");
+        
+        let objectLength = findObjectStringLength(currentlyUnsearchedString.substring(braceIndex, currentlyUnsearchedString.length));
+        if(objectLength < 0) //-1 means an error
+        {
+            return [true, errPreText + "Data addition tag was not properly closed."];
+        }
+        
+        
+        
+        //Now try to turn the tag's contents into a parsable object
+        let newData;
+        try
+        {
+            //The current tag has the '+' characters, so don't include them
+            newData = JSON.parse("{" + currentlyUnsearchedString.substring(braceIndex + 2, braceIndex + objectLength - 2) + "}");
+        }
+        catch(err)
+        {
+            return [true, errPreText + err];
+        }
+        
+        
+        //Now just add the data to the data object (the new values are priority if that property already exists)
+        dataObject = mergeObjects([newData, dataObject]);
+        
+        
+        //Finally, cut down the string so we don't try to re-add this tag.
+        currentlyUnsearchedString = currentlyUnsearchedString.substring(braceIndex + objectLength, currentlyUnsearchedString.length);
+    }
+    
+    return [false, dataObject];
+}
+
+export default addDataObjectAdditionsFromTemplate;
---
+++ debug/src/addDataObjectAdditionsFromTemplate/addDataObjectAdditionsFromTemplate.test.js
@@ -0,0 +1,88 @@
+import addDataObjectAdditionsFromTemplate from './addDataObjectAdditionsFromTemplate';
+
+test("addDataObjectAdditionsFromTemplate will return an error if the addition tag doesn't have an end", () => {
+    
+    const template = 'Test. {+ "newData": "some new data" '; //Not ended with +}
+    const dataObject = {
+        someData: "some current data"
+    };
+    
+    const [isError, result] = addDataObjectAdditionsFromTemplate(dataObject, template);
+    
+    expect(typeof isError).toBe("boolean");
+    expect(typeof result).toBe("string");
+    
+    expect(isError).toBeTruthy();
+});
+
+
+test("addDataObjectAdditionsFromTemplate will return an error if the addition tag doesn't contain a valid object", () => {
+    
+    const template = 'Test. {+ jhsjdkfhjkdhfjshfjkshf +}';
+    const dataObject = {
+        someData: "some current data"
+    };
+    
+    const [isError, result] = addDataObjectAdditionsFromTemplate(dataObject, template);
+    
+    expect(typeof isError).toBe("boolean");
+    expect(typeof result).toBe("string");
+    
+    expect(isError).toBeTruthy();
+});
+
+
+test("addDataObjectAdditionsFromTemplate will return the object with both old and new data, when the addition works", () => {
+    
+    const template = 'Test. {+ "newData": "some new data" +}';
+    const dataObject = {
+        someData: "some current data"
+    };
+    
+    const [isError, result] = addDataObjectAdditionsFromTemplate(dataObject, template);
+    
+    expect(typeof isError).toBe("boolean");
+    
+    expect(isError).toBeFalsy();
+    expect(result.hasOwnProperty("someData")).toBeTruthy();
+    expect(result.someData).toBe("some current data");
+    expect(result.hasOwnProperty("newData")).toBeTruthy();
+    expect(result.newData).toBe("some new data");
+})
+
+
+test("addDataObjectAdditionsFromTemplate will return an object with data from multiple tags", () => {
+    
+    const template = '{+ "newData1": "some new data" +} Test. {+ "newData2": "some other data" +}';
+    const dataObject = {
+        someData: "some current data"
+    };
+    
+    const [isError, result] = addDataObjectAdditionsFromTemplate(dataObject, template);
+    
+    expect(typeof isError).toBe("boolean");
+    
+    expect(isError).toBeFalsy();
+    expect(result.hasOwnProperty("someData")).toBeTruthy();
+    expect(result.someData).toBe("some current data");
+    expect(result.hasOwnProperty("newData1")).toBeTruthy();
+    expect(result.newData1).toBe("some new data");
+    expect(result.hasOwnProperty("newData2")).toBeTruthy();
+    expect(result.newData2).toBe("some other data");
+})
+
+
+test("If the data object already contains this property, addDataObjectAdditionsFromTemplate will replace with the new value", () => {
+    
+    const template = 'Test. {+ "someData": "some new data" +}';
+    const dataObject = {
+        someData: "some current data"
+    };
+    
+    const [isError, result] = addDataObjectAdditionsFromTemplate(dataObject, template);
+    
+    expect(typeof isError).toBe("boolean");
+    expect(isError).toBeFalsy();
+    expect(result.hasOwnProperty("someData")).toBeTruthy();
+    expect(result.someData).toBe("some new data");
+})
