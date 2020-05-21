d5cb0639d4df73b7fbc6009091e42dfbcb4250fd
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-05-21 11:48:52 +0100

    some issues with inner templates not having escaped double-quotes. These were templates used by the tests for scriptKnapperMain, so a lot of tests were failing for that reason, as it couldn't be parsed properly

--- debug/src/scriptKnapperMain/scriptKnapperMain.test.js
+++ debug/src/scriptKnapperMain/scriptKnapperMain.test.js
@@ -1,6 +1,6 @@
 import scriptKnapperMain from './scriptKnapperMain';
 
-const templateObjects = String.raw`[{"name": "simpleTemplate","template": "this is {: data1 :}, and that is {:data2:}, and over there is {:data3 :}. Watch out for @ohb:, @chb:, @odhb:, @cdhb:, @ohb+ and @chb+ you know."},{"name": "templateLayer2","template": "this is some more data here: {:data1:}"},{"name": "noDataTemplate","template": "I don't need any data."}, {"name": "dataAdditionTemplate", "template": "Test {+ "testProp": "test" +} addition. {:testProp:}"}, {"name": "badDataAdditionTemplate", "template": "Test {+ "testProp": "test"  addition."}]`;
+const templateObjects = String.raw`[{"name": "simpleTemplate","template": "this is {: data1 :}, and that is {:data2:}, and over there is {:data3 :}. Watch out for @ohb:, @chb:, @odhb:, @cdhb:, @ohb+ and @chb+ you know."},{"name": "templateLayer2","template": "this is some more data here: {:data1:}"},{"name": "noDataTemplate","template": "I dont need any data."}, {"name": "dataAdditionTemplate", "template": "Test {+ \"testProp\": \"test\" +} addition. {:testProp:}"}, {"name": "badDataAdditionTemplate", "template": "Test {+ \"testProp\": \"test\"  addition."}]`;
 
 //-----------------------------------------------------------------------------------
 
@@ -64,7 +64,7 @@ test("scriptKnapperMain will generate a template once, with an empty data object
     expect(typeof resultText).toBe("string");
     
     expect(resultError).toBeFalsy();
-    expect(resultText).toContain("I don't need any data."); //Found in the "noDataTemplate" template
+    expect(resultText).toContain("I dont need any data."); //Found in the "noDataTemplate" template
 });
 
 // -----------------------------------------------------------------------------------
@@ -219,42 +219,6 @@ test("scriptKnapperMain will return an error if a given embedded template reques
     expect(typeof resultText).toBe("string");
     
     expect(resultError).toBeTruthy();
-});
-
-test("scriptKnapperMain will return an error if a given embedded template request object doesn't have the right properties", () => {
-    
-    const templateName = "simpleTemplate";
-    
-    //No data
-    const markup1 = {
-        template: templateName
-    };
-    
-    // No template
-    const markup2 = {
-        data: [
-            { 
-                data1: "theData1", 
-                data2: "testTemplate: {{: sjdhfsjdhfkjsdhf :}} - This is now the string again. ",
-                data3: "theData3" 
-            }
-        ]
-    };
-    
-    const markupObject1 = JSON.stringify(markup1);
-    
-    const markupObject2 = JSON.stringify(markup2);
-    
-    const [resultError1, resultText1] = checkForMarkupObjectError(markupObject1, templateObjects);
-    const [resultError2, resultText2] = checkForMarkupObjectError(markupObject2, templateObjects);
-    
-    expect(typeof resultError1).toBe("boolean");
-    expect(typeof resultText1).toBe("string");
-    expect(typeof resultError2).toBe("boolean");
-    expect(typeof resultText2).toBe("string");
-    
-    expect(resultError1).toBeTruthy();
-    expect(resultError2).toBeTruthy();
 });
 
 test("scriptKnapperMain will return an error caught by the checkForMarkupObjectError function", () => {
