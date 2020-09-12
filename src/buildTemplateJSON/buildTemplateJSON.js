d6da3f45478a30273234bfcb886db170746710c3
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-09-12 15:08:40 +0100

    setup and tested the buildTemplateJSON function

---
+++ src/buildTemplateJSON/buildTemplateJSON.js
@@ -0,0 +1,50 @@
+import replaceSubStrings from '../replaceSubStrings/replaceSubStrings';
+
+/*
+    Inputs:
+        - templates: An array of objects. Each object will have 2 properties:
+			a "templateName" and a "template" (which is the text of the template).
+		- templatesAlreadyPrepared: A boolean value. Has the template text already
+			been prepared?
+        
+    Output:
+        - This function will return a JSON string, which will contain all of the
+			given templates, in the structure required by scriptKnapperMain(). 
+			If templatesAlreadyPrepared is true, double quote characters will be 
+			escaped, and whitespace characters (such as tabs and new-lines, 
+			but not spaces) will be replaced with escape codes.
+    
+*/
+
+function buildTemplateJSON(templates, templatesAlreadyPrepared = false)
+{
+	let result = "[";
+	
+	templates.forEach(template => {
+		
+		let templateText = template.template;
+		
+		if(!templatesAlreadyPrepared)
+		{
+			templateText = replaceSubStrings(
+				templateText,
+				[
+					{ from: "\t", to: String.raw`\t` },
+					{ from: "\v", to: String.raw`\v` },
+					{ from: "\r", to: String.raw`\r` },
+					{ from: "\n", to: String.raw`\n` },
+					{ from: '"', to: String.raw`\"` }
+				]
+			);
+		}
+		
+		//If this isn't the first element in the array, add a comma after the previous element
+		if(result.charAt(result.length - 1) === "}") result += ",";
+		
+		result += '\n\t{ "name": "' + template.templateName + '", "template": "' + templateText + '" }';
+	});
+	
+	return result + "\n]";
+}
+
+export default buildTemplateJSON;
---
+++ src/buildTemplateJSON/buildTemplateJSON.test.js
@@ -0,0 +1,73 @@
+import buildTemplateJSON from './buildTemplateJSON.js';
+
+const inputBasicTemplate1Name = "test1";
+const inputBasicTemplate1 = "test test test test";
+
+const inputBasicTemplate2Name = "test2";
+const inputBasicTemplate2 = String.raw`There once was a man from Nantucket.
+He had a chicken nugget.
+	He ate it neat.
+	It tasted sweet.
+Then he went and bought a whole bucket.`;
+const inputBasicTemplate2Prepared = String.raw`There once was a man from Nantucket.\nHe had a chicken nugget.\n\tHe ate it neat.\n\tIt tasted sweet.\nThen he went and bought a whole bucket.`;
+
+const inputBasicTemplate3Name = "test3";
+const inputBasicTemplate3 = String.raw`There once was a man from Nantucket.
+He had a chicken nugget.
+	He ate it "quick".
+	It tasted "sick".
+Then he went and bought a whole bucket.`;
+const inputBasicTemplate3Prepared = String.raw`There once was a man from Nantucket.\nHe had a chicken nugget.\n\tHe ate it \"quick\".\n\tIt tasted \"sick\".\nThen he went and bought a whole bucket.`;
+
+const inputPreparedTemplateName = "testPrepared";
+const inputPreparedTemplate = 'There once was a man from Nantucket.\nHe had a chicken nugget.\n\tHe ate it "quick".\n\tIt tasted "sick".\nThen he went and bought a whole bucket.';
+
+
+test("buildTemplateJSON() will return an empty array string, if input array has 0 objects", () => {
+	
+	const result = buildTemplateJSON([]);
+	expect(result).toEqual("[\n]");
+});
+
+test("buildTemplateJSON() will return json in the correct structure, with the given values, for 1 template", () => {
+    
+    const result = buildTemplateJSON([
+		{ templateName: inputBasicTemplate1Name, template: inputBasicTemplate1 }
+	]);
+	
+	expect(result).toEqual('[\n\t{ "name": "' + inputBasicTemplate1Name + '", "template": "' + inputBasicTemplate1 + '" }\n]');
+    
+});
+
+test("buildTemplateJSON() will return json in the correct structure, with the given values, for all templates", () => {
+    
+    const result = buildTemplateJSON([
+		{ templateName: "test1", template: inputBasicTemplate1 },
+		{ templateName: "test2", template: inputBasicTemplate1 },
+		{ templateName: "test3", template: inputBasicTemplate1 }
+	]);
+	
+	expect(result).toEqual('[\n\t{ "name": "test1", "template": "' + inputBasicTemplate1 + '" },\n\t{ "name": "test2", "template": "' + inputBasicTemplate1 + '" },\n\t{ "name": "test3", "template": "' + inputBasicTemplate1 + '" }\n]');
+    
+});
+
+test("buildTemplateJSON() will replace the whitespace, and escape double quotes, in the given templates if templatesAlreadyPrepared is false", () => {
+    
+    const result = buildTemplateJSON([
+		{ templateName: inputBasicTemplate1Name, template: inputBasicTemplate1 },
+		{ templateName: inputBasicTemplate2Name, template: inputBasicTemplate2 },
+		{ templateName: inputBasicTemplate3Name, template: inputBasicTemplate3 }
+	]);
+	
+	expect(result).toEqual('[\n\t{ "name": "' + inputBasicTemplate1Name + '", "template": "' + inputBasicTemplate1 + '" },\n\t{ "name": "' + inputBasicTemplate2Name + '", "template": "' + inputBasicTemplate2Prepared + '" },\n\t{ "name": "' + inputBasicTemplate3Name + '", "template": "' + inputBasicTemplate3Prepared + '" }\n]');
+    
+    
+});
+
+test("buildTemplateJSON() will not make replacements in the template if templatesAlreadyPrepared is true", () => {
+    
+    const result = buildTemplateJSON([{ templateName: inputPreparedTemplateName, template: inputPreparedTemplate }], true);
+    
+	expect(result).toEqual('[\n\t{ "name": "' + inputPreparedTemplateName + '", "template": "' + inputPreparedTemplate + '" }\n]');
+	
+});
