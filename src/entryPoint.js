e7ad3c541a6d444ca432829f1c3a7c7fb2cb627e
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2021-09-09 14:52:06 +0100

    setup the entry point script, and tested

---
+++ src/entryPoint.js
@@ -0,0 +1,5 @@
+import scriptKnapperMain from './scriptKnapperMain/scriptKnapperMain';
+import prepareTemplateString from './prepareTemplateString/prepareTemplateString';
+import buildTemplateJSON from './buildTemplateJSON/buildTemplateJSON';
+
+module.exports = { scriptKnapperMain, prepareTemplateString, buildTemplateJSON };
---
+++ src/entryPoint.test.js
@@ -0,0 +1,16 @@
+import sk from './entryPoint';
+import scriptKnapperMain from './scriptKnapperMain/scriptKnapperMain';
+import prepareTemplateString from './prepareTemplateString/prepareTemplateString';
+import buildTemplateJSON from './buildTemplateJSON/buildTemplateJSON';
+
+test("entryPoint is exporting the right function for scriptKnapperMain", () => {
+	expect(sk.scriptKnapperMain.toString()).toEqual(scriptKnapperMain.toString());
+});
+
+test("entryPoint is exporting the right function for prepareTemplateString", () => {
+	expect(sk.prepareTemplateString.toString()).toEqual(prepareTemplateString.toString());
+});
+
+test("entryPoint is exporting the right function for buildTemplateJSON", () => {
+	expect(sk.buildTemplateJSON.toString()).toEqual(buildTemplateJSON.toString());
+});
